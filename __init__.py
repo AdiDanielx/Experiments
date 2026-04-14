acda7f51bf3a0d39dc8e10581544799cf4f48db2
Adi Daniel <124789363+AdiDanielx@users.noreply.github.com>
2026-04-14 10:50:19 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,6 @@
+"""OCDS: Online Concept Drift Detection and Streaming"""
+
+from OCDS.OCDS import OCDSClassifier
+from OCDS.ocds_feature_comparison import OCDSFeatureComparison
+
+__all__ = ['OCDSClassifier', 'OCDSFeatureComparison']
