fc8e69f83c829bdd414befbb3f4191007c66780b
Adi Daniel <124789363+AdiDanielx@users.noreply.github.com>
2026-04-14 10:49:12 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,6 @@
+"""GBDT-IL: Gradient Boosting Decision Tree with Incremental Learning"""
+
+from GBDTIL import GBDTILClassifier
+from GBDTIL.gbdtil_feature_comparison import GBDTILFeatureComparison
+
+__all__ = ['GBDTILClassifier', 'GBDTILFeatureComparison']
