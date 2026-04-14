0cb50cab912fe15e5c934fb59d7f399b85d46785
Adi Daniel <124789363+AdiDanielx@users.noreply.github.com>
2026-04-14 10:48:17 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,6 @@
+"""EML: Ensemble of Meta-Learners"""
+
+from EML.EML import EMLClassifier
+from EML.eml_feature_comparison import EMLFeatureComparison
+
+__all__ = ['EMLClassifier', 'EMLFeatureComparison']
